f29122e4fca7a6aeafecb07d77226e10e1c4a8ab
Peltea Bogdan <mhibogdan@gmail.com>
2020-04-29 18:36:21 +0300

    StartPage Responsive + grid fixed + svg resources

---
+++ Scripts/mobileMenu.js
@@ -0,0 +1,8 @@
+function openPopout() {
+    document.querySelector('.bg-modal').style.display = "flex";
+
+}
+
+function closePopout() {
+    document.querySelector('.bg-modal').style.display = "none";
+}
--- StartPage.php
+++ StartPage.php
@@ -7,13 +7,17 @@
         <title>TeVi</title>
     </head>
     <body>
+          
+<script src="Scripts/mobileMenu.js"></script>
+          
           <img class="img-left" src="Styles/Backgrounds/bg-left.svg">
           <img class="img-right" src="Styles/Backgrounds/bg-right.svg">
+          <img class="img-right-mobile" src="Styles/Backgrounds/bg-right-mobile.svg">
 
 
 
               <ul class="nav-area">
-                <li><a href="StartPage.php">home</a></li>
+                <li><a href="HomePage.php">home</a></li>
                 <li><a>|</a></li>
                 <li><a href="Stats.php">statistics</a></li>
                 <li><a>|</a></li>
@@ -21,18 +25,44 @@
             </ul>
   
 
+          
+             <button onclick="openPopout()" id="menu" class="mobile-menu" type="button"></button>
+    
+
+    <!--    Pop-out menur-->
+    <div class="bg-modal">
+        <div class="modal-contents">
+
+            <div onclick="closePopout()" class="close">+</div>
+            <form action="">
+                <ul class="mobile-menu-text">
+                <li><a href="HomePage.php">home</a></li>
+                <li><a href="Stats.php">statistics</a></li>
+                <li><a href="Resources.php">resources</a></li>
+
+                </ul>
+
+            </form>
+</div>
+    </div>
+<!--    -->      
+
+
+
     <div class="wrapper">
         <div class="info-page">
           <h5>TeVi</h5>
           <p>A global terrorism <br>visualizer</p>
         </div>
         <div clas="button-wrap">
-        <button onclick="location.href='HomePage.php'" type="button">Get started</button>
+        <button onclick="location.href='HomePage.php'" class="get-started-button" type="button">Get started</button>
           </div>
         <div class="extra-info">
         <p >TeVi is a flexible Web data visualizer regarding worldwide terrorism, based on our own RESTful API. The statistics, views and maps can be exported as CSV, PNG or SVG.</p>
       </div>
     </div>
+
+
 
 
     </body>
