f9341a22c0eb9ee048b64d94f72f242f8101d800
Victor <44725192+Victronic@users.noreply.github.com>
2020-04-29 16:27:10 +0300

    Idk what's happening

--- HomePage.php
+++ HomePage.php
@@ -4,29 +4,34 @@
         <meta charset="utf-8">
         <title> TeVi - Home Page </title>
         <link rel="icon" type="image/ico" href="Images/SEO.png" />
-        <link rel="stylesheet" href="Styles/HomePage.css">
+        <link rel="stylesheet" href="Styles/HomePage.css"> 
 
         <meta name="terrorism" content="all">
         <meta name="author" content="Irina Calin, Victor-Ionut Platon and Bogdan-Mihail Peltea">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="A web global terrorism visualizer">
     </head>
+
     <body id="whole_body">
-        <div class="menu-info">
-          <img class="img-logo" src="Styles/Backgrounds/LOGO.svg">
+        
+        <nav class="menu-info">
+            <img class="img-logo" src="Styles/Backgrounds/LOGO.svg">
             <ul class="nav-area">
-                <li><a href="" class="home">home</a></li>
-                <li><a href="Stats.php">statistics</a></li>
-                <li><a href="Resources.php">resources</a></li>
+                <li><a href="Homepage.php" id="active">HOME</a></li>
+                <li><a href="Stats.php">STATISTICS</a></li>
+                <li><a href="Resources.php">RESOURCES</a></li>
             </ul>
-        </div>
+        </nav>
+
         <div class="page_wrapper">
+            
             <header class="upper_intro">
                 <div class="page_title">
                     <h1> TeVi </h1>
                     <h2> A GLOBAL TERRORISM VISUALIZER </h2>
                 </div>
             </header>
+
             <section class="summary">
                 <div class="text">
                     <h3>Short Summary - Abstract </h3>
