d524b89df86035229d2c20c7ddfb11cbf89f47e7
Saranoja <Saranoja@users.noreply.github.com>
2020-05-06 11:17:54 +0300

    Update on the database scripts -  populated with data

--- Database/EchoDatabase.php
+++ Database/EchoDatabase.php
@@ -1,29 +1,22 @@
 <?php
-$row = 1;
-$line = array();
+
+// script used in order to check the number of fields of the original dataset
+
+$row = 0;
 
 if (($handle = fopen("globalterrorismdb_0718dist.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    while (($data = fgetcsv($handle, 3000, ",")) !== FALSE) {
         $num = count($data);
-        array_push($line, $data);
-
-        // if($num > 134) {
-        //     for ($col = 0; $col <= 134; $col++) {
-                
-        //     }
-
-
-
-        //     unset($line);
-        //     $line = array();
-        // }
 
         echo "<p> $num fields in line $row: <br /></p>\n";
         $row++;
-        // for ($c=0; $c < $num; $c++) {
-        //     echo $data[$c] . "<br />\n";
-        // }
 
+        // if($row > 2) {
+        
+        //     // for ($c=0; $c < $num; $c++) {
+        //     //     echo $data[$c] . "<br />\n";
+        //     // }
+        // }
     }
     fclose($handle);
 }
