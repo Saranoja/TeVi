102353b9501a255d2782f6590cbdd2effa848708
Saranoja <Saranoja@users.noreply.github.com>
2020-05-27 14:10:24 +0300

    Branch init

--- app/models/Attacks.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo 'Hello world, this is the Attacks model';
--- app/views/StatsControllers/CriteriaControl.php
+++
@@ -1,6 +0,0 @@
-<?php
-function getClasses() {
-    return ["event", "time", "location", "attacker", "target", "weapon" , "deaths"];
-}
-$all_classes = getClasses();
-?>
